e19c3a053b68abe3e8acc933ab216030f0aa5091
ggingembre@gmail.com <Sailing2010>
2017-02-07 10:45:16 +0200

    Commit 1 Homework 1 bird class

---
+++ src/Module3_Homework1.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Guillaume Gingembre on 06/02/2017.
+ */
+public class Module3_Homework1 {
+    public static void main(String[] args) {
+
+        bird birdObject = new bird();
+
+        birdObject.walk();
+        birdObject.fly();
+        birdObject.sing();
+        birdObject.whoAmI();
+
+    }
+}
+
+// uses the bird public class and its methods created in another file called "bird".
---
+++ src/bird.java
@@ -0,0 +1,22 @@
+/**
+ * Created by Guillaume Gingembre on 06/02/2017.
+ */
+public class bird {
+
+        public void walk(){
+            System.out.println("I am walking");
+        }
+
+        public void fly (){
+            System.out.println("I am flying");
+        }
+
+        public void sing(){
+            System.out.println("I am singing");
+        }
+
+        public void whoAmI(){
+            System.out.println("I am a bird");
+        }
+
+}
