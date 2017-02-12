3d51c782b1513a8172ac35fc70736831b347e509
ggingembre@gmail.com <Sailing2010>
2017-02-12 13:35:52 +0200

    Commit 5 bird assignment done

--- src/BirdHomework/Module3_Homework1.java
+++ src/BirdHomework/Module3_Homework1.java
@@ -5,15 +5,19 @@ package BirdHomework;
  */
 public class Module3_Homework1 {
     public static void main(String[] args) {
+        String Songs [] = new String[4];
+        int i;
+
+        Songs[0] = "I am walking";
+        Songs[1] = "I am flying";
+        Songs[2] = "I am singing";
+        Songs[3] = "I am a Bird";
 
         bird birdObject = new bird();
 
-       // birdObject.walk();
-        // birdObject.fly();
-        birdObject.sing();
-        // birdObject.whoAmI();
+        for (i = 0; i < Songs.length; i++) {
+            birdObject.sing(Songs[i]);
+        }
 
     }
 }
-
-// uses the BirdHomework.bird public class and its methods created in another file called "BirdHomework.bird".
--- src/BirdHomework/bird.java
+++ src/BirdHomework/bird.java
@@ -3,23 +3,12 @@ package BirdHomework;
 /**
  * Created by Guillaume Gingembre on 06/02/2017.
  */
+
+
+
 public class bird {
 
-       // public void walk(){
-       //     System.out.println("I am walking");
-       // }
-
-        // public void fly (){
-        //    System.out.println("I am flying");
-        //}
-
-       // public void sing(){
-       //     System.out.println("I am singing");
-       // }
-
-        public void sing(){
-            String Song;
-            Song = "I am walking,\n" + "I am singing, \n" +  "I am flying, \n" + "I am a BirdHomework.bird";
+        public void sing(String Song){
             System.out.println(Song);
         }
 
