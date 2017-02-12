8b22a461880bad5addc36adf6e46275b5344cd55
ggingembre@gmail.com <Sailing2010>
2017-02-12 17:18:07 +0200

    Commit 7 submission of assignment 3.4.

---
+++ src/Bank/BankSolution.java
@@ -0,0 +1,29 @@
+package Bank;
+
+/**
+ * Created by Guillaume Gingembre on 12/02/2017.
+ */
+public class BankSolution {
+    public static void main(String[] args) {
+
+        // create new user
+        User Guillaume = new User ("Gingembre",10000,
+                1, "GoIT",1000,"EUR");
+
+        // print new user before changes
+        System.out.println(Guillaume.toString());
+
+        // test the companyNameLength method
+        System.out.println("\nThe length of the name of the company where Guillaume works (" +
+        Guillaume.getCompanyName() + ") is: " + Guillaume.companyNameLength()+". \n");
+
+        // make changes calling methods from user class
+        Guillaume.paySalary();
+        Guillaume.withdraw(500);
+        Guillaume.monthIncreaser(1);
+
+        // check results by printing Guillaume after changes
+        System.out.println(Guillaume.toString());
+
+    }
+}
---
+++ src/Bank/User.java
@@ -0,0 +1,101 @@
+package Bank;
+
+/**
+ * Created by Guillaume Gingembre on 12/02/2017.
+ */
+public class User {
+    private String name;
+    private int balance;
+    private int monthsOfEmployment;
+    private String companyName;
+    private int salary;
+    private String currency;
+
+    public User(String name, int balance, int monthsOfEmployment, String companyName, int salary, String currency){
+        this.name = name;
+        this.balance = balance;
+        this.monthsOfEmployment = monthsOfEmployment;
+        this.companyName = companyName;
+        this.salary = salary;
+        this.currency = currency;
+    }
+
+    public void paySalary(){
+        balance = balance + salary;
+    }
+
+    public void withdraw(int summ){
+        double commissions;
+
+        if (summ<1000){
+            commissions = summ*0.05;
+        } else {
+            commissions = summ * 0.1;
+        }
+
+        balance = (int) (balance - summ - commissions);
+    }
+
+    public int companyNameLength(){
+        return this.companyName.length();
+    }
+
+    public void monthIncreaser(int addMonth){
+        this.monthsOfEmployment = this.monthsOfEmployment + addMonth;
+    }
+
+    @Override // in order to properly print the objects
+    public String toString() {
+        return "Name: " + name  + "\nBalance: " + (balance) + "\nMonths of Employment: " +
+                monthsOfEmployment + "\nCompany Name: " + companyName + "\nSalary: " + salary + "\nCurrency: "
+                + currency;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public int getMonthsOfEmployment() {
+        return monthsOfEmployment;
+    }
+
+    public void setMonthsOfEmployment(int monthsOfEmployment) {
+        this.monthsOfEmployment = monthsOfEmployment;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+}
